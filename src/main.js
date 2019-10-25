46f08fbb59e9aa303c9b92d1741950c888700874
Roy-9L <l>
2019-10-25 21:57:59 +0800

    使用element-ui库

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue'
-
+// ElementUI 是一个插件  集成了很多组件
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 // 根实例，
