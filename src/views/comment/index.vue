274f2f1fffb59252b52ac4b5e3410d015f9b2f1f
Roy-9L <l>
2019-11-06 23:47:31 +0800

    评论管理模块完成

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -1,15 +1,102 @@
 <template>
-  <div class="container">
+  <div class="container-comment">
     <el-card>
       <div slot="header">
         <my-bread>评论管理</my-bread>
       </div>
+      <!-- 表格 -->
+      <el-table :data="articles">
+        <el-table-column label="标题" width="400" prop="title"></el-table-column>
+        <el-table-column label="总评论数" prop="total_comment_count"></el-table-column>
+        <el-table-column label="粉丝评论数" prop="fans_comment_count"></el-table-column>
+        <el-table-column label="状态">
+          <template slot-scope="scope">{{scope.row.comment_status?'正常':'关闭'}}</template>
+        </el-table-column>
+        <el-table-column label="操作" width="120" prop>
+          <template slot-scope="scope">
+            <el-button
+              @click="toggleStatus(scope.row)"
+              v-if="scope.row.comment_status"
+              type="danger"
+              size="small"
+            >关闭评论</el-button>
+            <el-button @click="toggleStatus(scope.row)" v-else type="success" size="small">打开评论</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <!-- 分页 -->
+      <el-pagination
+        style="margin-top:20px"
+        background
+        layout="prev,pager,next"
+        :total="total"
+        :page-size="reqParams.per_page"
+        :current-page="reqParams.page"
+        @current-change="pager"
+      ></el-pagination>
     </el-card>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      // 文章列表
+      articles: [],
+      // 请求的参数
+      reqParams: {
+        response_type: 'comment',
+        page: 1,
+        per_page: 20
+      },
+      // 总条数  默认0
+      total: 0
+    }
+  },
+  created () {
+    this.getArticles()
+  },
+  methods: {
+    // 切换评论状态  正常  关闭
+    toggleStatus (row) {
+      // row 有id    comment_status
+      // const { id, comment_status } = row   !!!不能这么写，语法上过不去
+      // 动态生成文案
+      const text = row.comment_status ? '您确认关闭该文章的评论功能吗？如果关闭，用户将不能对该文章进行评论' : '您确认打开评论功能吗？'
+      // 打开对话框
+      this.$confirm(text, '温馨提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(async () => {
+        // 点击确认时 发修改状态的请求
+        const { data: { data } } = await this.$axios.put(`comments/status?article_id=${row.id}`, {
+          allow_comment: !row.comment_status
+        })
+        // 成功
+        // allow_comment   后端返回的最新状态
+        this.$message.success(data.allow_comment ? '打开评论成功' : '关闭评论成功')
+        // 更新当前文章的状态
+        row.comment_status = data.allow_comment
+      }).catch(() => {
+        // 点击取消时 不做任何事情
+      })
+    },
+    async getArticles () {
+      const { data: { data } } = await this.$axios.get('articles', { params: this.reqParams })
+      this.articles = data.results
+      this.total = data.total_count
+    },
+    // 分页函数
+    pager (newPage) {
+      // 修改当前的页码为新的页码
+      this.reqParams.page = newPage
+      // 重新获取数据
+      this.getArticles()
+    }
+  }
+}
 </script>
 
 <style scoped lang='less'></style>
